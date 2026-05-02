108d37f2a3ce8585a7450e2b0711e6e8e31c68b9
ernest <ernest_s@bk.ru>
2026-05-02 13:40:06 +0330

    fix paths for stt_model

--- STT_model/Speech-to-Text-main/config.py
+++ STT_model/Speech-to-Text-main/config.py
@@ -1,35 +1,22 @@
-"""
-config.py — централизованная конфигурация путей и параметров.
-
-Измени BASE_DIR под своё окружение:
-  - Linux/Ubuntu: Path.home() / "Tom_D/CoursePaper"
-  - Windows:      Path("D:/CoursePaper")
-"""
-
 from pathlib import Path
-import platform
+import os
+from dotenv import load_dotenv
 
-if platform.system() == "Windows":
-    BASE_DIR = Path("D:/CoursePaper")
-else:
-    BASE_DIR = Path.home() / "PyCharmProjects/VoiceMessageKnowledge/stt_model"
+load_dotenv(Path(__file__).parent.parent.parent / ".env")
 
-RECORD_DIR = Path.home() / "PyCharmProjects/VoiceMessageKnowledge/data/my_recorded_waw"
-MODEL_DIR = BASE_DIR / "model" / "wav2vec2_golos_002"
-OUTPUT_DIR = Path.home() / "PyCharmProjects/VoiceMessageKnowledge/data/final_texts"
+MODEL_DIR = Path(os.getenv("STT_MODEL_DIR"))
+OUTPUT_DIR = Path(__file__).parent.parent.parent / Path(os.getenv("STT_OUTPUT_DIR"))
+RECORD_DIR = Path(__file__).parent.parent.parent / Path(os.getenv("STT_RECORD_DIR"))
 OUTPUT_FILE = OUTPUT_DIR / "transcript.txt"
-SILERO_DIR = BASE_DIR / "silero_vad" / "silero-vad-master"
-LM_PATH = BASE_DIR / "lm" / "ru_3gram.bin"
+SILERO_DIR = Path(os.getenv("SILERO_DIR"))
+LM_PATH = Path(os.getenv("LM_PATH"))
 
-# ASR параметры
 MIN_SPEAKERS = 1
 MAX_SPEAKERS = 1
 SAMPLE_RATE = 16000
 
-# Диаризация
 MONOLOGUE_STD_THRESHOLD = 0.135
 
-# Beam search
 USE_LM = False
 LM_ALPHA = 0.5
 LM_BETA = 1.5
@@ -40,8 +27,6 @@ DEFAULT_HOTWORDS = [
     "краснодар", "университет", "институт", "факультет",
 ]
 
-# Пунктуация
 USE_PUNCTUATION = True
 
-RECORD_DIR.mkdir(parents=True, exist_ok=True)
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
